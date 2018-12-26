6013deea614a37ff249ab34657e3ac329726ee0b
Annmary <annmaryamaka@gmail.com>
2018-12-26 21:57:09 +0100

    implemnt navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+// pages
+import { HomeComponent } from './pages/home/home.component';
+
+// components
+import { NavBarComponent } from './components/NavBar/nav-bar.component';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+  BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
